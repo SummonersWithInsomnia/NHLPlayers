661467aa1b3ba43cb2e5880bebe0eccb914af1fe
Yu <yu@hiya.moe>
2024-10-28 15:15:03 -0600

    Add the NHL player data structure

---
+++ Player.cs
@@ -0,0 +1,27 @@
+﻿namespace NHLPlayers
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Team { get; set; }
+        public string Pos { get; set; }
+        public double GP { get; set; }
+        public double G { get; set; }
+        public double A { get; set; }
+        public double P { get; set; }
+        public double PlusOrMinus { get; set; }
+        public double PIM { get; set; }
+        public string PPerGP { get; set; }
+        public double PPG { get; set; }
+        public double PPP { get; set; }
+        public double SHG { get; set; }
+        public double SHP { get; set; }
+        public double GWG { get; set; }
+        public double OTG { get; set; }
+        public double S { get; set; }
+        public double SPercentage { get; set; }
+        public string TOIPerGP { get; set; }
+        public double ShiftsPerGP { get; set; }
+        public double FOWPercentage { get; set; }
+    }
+}
